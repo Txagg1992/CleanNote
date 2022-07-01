85ad055d4bbcc57e28715c4a6212760bf9fcc851
Enoch Phetteplace <curiousaps@gmail.com>
2022-06-30 23:54:40 -0500

    Update Kotlin.

--- app/src/main/java/com/curiousapps/cleannote/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/curiousapps/cleannote/feature_note/presentation/notes/NotesViewModel.kt
@@ -2,11 +2,9 @@ package com.curiousapps.cleannote.feature_note.presentation.notes
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.curiousapps.cleannote.feature_note.domain.model.Note
-import com.curiousapps.cleannote.feature_note.domain.use_case.GetNotesUseCase
 import com.curiousapps.cleannote.feature_note.domain.use_case.NoteUseCases
 import com.curiousapps.cleannote.feature_note.domain.util.NoteOrder
 import com.curiousapps.cleannote.feature_note.domain.util.OrderType
